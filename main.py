1c13224c70a24a47a3be1c3911356f3c9f084eb1
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-12-13 09:36:00 +0000

    @TotallyNotElite wouldn't shut up

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ if platform.system() == 'Windows':
 else:
 	clear = 'clear'
 
-with open("array.txt") as textFile:
-	cells = [line.split() for line in textFile]
+run = False
+cells = []
+try:
+	with open(input('Filename: ')) as textFile:
+		cells = [line.split() for line in textFile]
+		run = True
+except FileNotFoundError:
+	print('No such file in directory.')
+
+if run:
 	for x in range(len(cells)):
 		for y in range(len(cells[0])):
 			if cells[x][y] == '_':
@@ -20,41 +28,15 @@ with open("array.txt") as textFile:
 			elif cells[x][y] == '1':
 				cells[x][y] = '■'
 
-def alive(x,y):
-	num_of_neighbours = 0
-	neighbours = ((1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1))
-	for xx,yy in neighbours:
-		try:
-			if cells[x+xx][y+yy] == '■' and x+xx in range(len(cells)) and y+yy in range(len(cells[0])):
-				num_of_neighbours += 1
-		except IndexError: None
-	return num_of_neighbours
-
-system(clear)
-print('__'*len(cells[0]))
-for i in range(len(cells)):
-	print(*cells[i], end='')
-	print('|')
-print('‾‾'*len(cells[0]))
-print('Generation: {}'.format(gen))
-system('pause')
-
-while True:
-	dc_cells = deepcopy(cells)
-	for x in range(len(dc_cells)):
-		for y in range(len(dc_cells[0])):
-			live = alive(x,y)
-			if dc_cells[x][y] == '■' and live < 2:
-				dc_cells[x][y] = ' '
-			elif dc_cells[x][y] == '■' and (live == 2 or live == 3):
-				dc_cells[x][y] = '■'
-			elif dc_cells[x][y] == '■' and live > 3:
-				dc_cells[x][y] = ' '
-
-			if dc_cells[x][y] == ' ' and live == 3:
-				dc_cells[x][y] = '■'
-
-	cells = dc_cells
+	def alive(x,y):
+		num_of_neighbours = 0
+		neighbours = ((1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1))
+		for xx,yy in neighbours:
+			try:
+				if cells[x+xx][y+yy] == '■' and x+xx in range(len(cells)) and y+yy in range(len(cells[0])):
+					num_of_neighbours += 1
+			except IndexError: None
+		return num_of_neighbours
 
 	system(clear)
 	print('__'*len(cells[0]))
@@ -62,8 +44,34 @@ while True:
 		print(*cells[i], end='')
 		print('|')
 	print('‾‾'*len(cells[0]))
-	
-	gen += 1
 	print('Generation: {}'.format(gen))
-	if keyboard.is_pressed('space'):
-		system('pause')
+	system('pause')
+
+	while True:
+		dc_cells = deepcopy(cells)
+		for x in range(len(dc_cells)):
+			for y in range(len(dc_cells[0])):
+				live = alive(x,y)
+				if dc_cells[x][y] == '■' and live < 2:
+					dc_cells[x][y] = ' '
+				elif dc_cells[x][y] == '■' and (live == 2 or live == 3):
+					dc_cells[x][y] = '■'
+				elif dc_cells[x][y] == '■' and live > 3:
+					dc_cells[x][y] = ' '
+
+				if dc_cells[x][y] == ' ' and live == 3:
+					dc_cells[x][y] = '■'
+
+		cells = dc_cells
+
+		system(clear)
+		print('__'*len(cells[0]))
+		for i in range(len(cells)):
+			print(*cells[i], end='')
+			print('|')
+		print('‾‾'*len(cells[0]))
+	
+		gen += 1
+		print('Generation: {}'.format(gen))
+		if keyboard.is_pressed('space'):
+			system('pause')
