b68e1e94b013497c90bc88fa94b81310d7c7579f
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-12-28 19:58:26 +0000

    Add files via upload

--- opengof.py
+++ opengof.py
@@ -6,25 +6,21 @@ from OpenGL.GLUT import *
 from copy import deepcopy
 
 cells = []
+gen = 0
+
 try:
 	with open('array.txt') as txt:
 		cells = [line.split() for line in txt]
-		for x in range(len(cells)):
-			for y in range(len(cells[0])):
-				if cells[x][y] == '.':
-					cells[x][y] = ' '
-				elif cells[x][y] == 'o':
-					cells[x][y] = '■'
 except FileNotFoundError:
 	print('No such file in directory.')
 	sys.exit(0)
 
-def alive(x,y):
+def alive(x, y):
 	num_of_neighbours = 0
-	neighbours = ((1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1))
-	for xx,yy in neighbours:
+	neighbours = ((1,0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1))
+	for xx, yy in neighbours:
 		try:
-			if cells[x+xx][y+yy] == '■' and x+xx in range(len(cells)) and y+yy in range(len(cells[0])):
+			if cells[x+xx][y+yy] == 'o' and x+xx in range(len(cells)) and y+yy in range(len(cells[0])):
 				num_of_neighbours += 1
 		except IndexError: None
 	return num_of_neighbours
@@ -36,22 +32,21 @@ def string(x, y, text, color=(1.0, 1.0, 1.0)):
 	for ch in text:
 		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ctypes.c_int(ord(ch)))
 
-gen = 0
 def update(dt):
 	global cells, gen
 	dc_cells = deepcopy(cells)
 	for x in range(len(dc_cells)):
 		for y in range(len(dc_cells[0])):
 			live = alive(x,y)
-			if dc_cells[x][y] == '■' and live < 2:
-				dc_cells[x][y] = ' '
-			elif dc_cells[x][y] == '■' and (live == 2 or live == 3):
-				dc_cells[x][y] = '■'
-			elif dc_cells[x][y] == '■' and live > 3:
-				dc_cells[x][y] = ' '
+			if dc_cells[x][y] == 'o' and live < 2:
+				dc_cells[x][y] = '.'
+			elif dc_cells[x][y] == 'o' and (live == 2 or live == 3):
+				dc_cells[x][y] = 'o'
+			elif dc_cells[x][y] == 'o' and live > 3:
+				dc_cells[x][y] = '.'
 
-			if dc_cells[x][y] == ' ' and live == 3:
-				dc_cells[x][y] = '■'
+			if dc_cells[x][y] == '.' and live == 3:
+				dc_cells[x][y] = 'o'
 	cells = dc_cells
 	gen += 1
 
@@ -67,7 +62,7 @@ def on_draw():
 	glRotatef(-90.0, 0.0, 0.0, 1.0)
 	for x in range(len(cells)):
 		for y in range(len(cells[0])):
-			if cells[x][y] == ' ':
+			if cells[x][y] == '.':
 				glBegin(GL_QUADS)
 				glVertex2f(x*10, y*10)
 				glVertex2f(x*10, y*10+10)
